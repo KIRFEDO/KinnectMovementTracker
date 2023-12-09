fa48978c57df184d121504a3fdb11e835c9c0d42
KIRFEDO <46225321+KIRFEDO@users.noreply.github.com>
2023-12-09 23:28:15 +0100

    Finished migration of the app into the separate class

--- MovementTracker_GUI/App.cpp
+++ MovementTracker_GUI/App.cpp
@@ -1,8 +1,15 @@
 #include "App.h"
 #include "AppHelpers.h"
+#include <tuple>
+#include "d2d1.h"
+
+using namespace KinectAdapter;
 
 App::App(HINSTANCE hInstCurr, int nCmdShow)
 {
+	m_kinectAdapter = nullptr;
+	m_pDepthRGBX = nullptr;
+
 	this->m_hInstCurr = hInstCurr;
 	this->m_nCmdShow = nCmdShow;
 }
@@ -12,13 +19,17 @@ HRESULT App::run()
 	HRESULT hr;
 	hr = InitApp();
 
-	MSG msg;
+	MSG msg = { 0 };
 
 	// Main message loop:
-	while (GetMessage(&msg, nullptr, 0, 0))
+	while (WM_QUIT != msg.message)
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		UpdateKinectImage();
+		while (PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
 	}
 
 	return hr;
@@ -29,19 +40,48 @@ App::~App()
 	
 }
 
-HRESULT App::InitUI()
+HRESULT App::InitAndRegisterWindowClasses()
+{
+	HRESULT hr;
+
+	m_mainWindow.style = CS_HREDRAW | CS_VREDRAW;
+	m_mainWindow.lpfnWndProc = MainWindowProcessor;
+	m_mainWindow.hInstance = m_hInstCurr;
+	m_mainWindow.lpszClassName = m_mainWindowName;
+	m_mainWindow.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+
+	hr = RegisterClass(&m_mainWindow) == 0 ? E_FAIL : S_OK;
+	if (FAILED(hr))
+		return hr;
+
+	m_kinectViewWindow.lpszClassName = m_kinectViewWindowName;
+	m_kinectViewWindow.lpfnWndProc = KinectViewProcessor;
+	m_kinectViewWindow.hInstance = NULL;
+	
+	hr = RegisterClass(&m_kinectViewWindow);
+	if (FAILED(hr))
+		return hr;
+
+	auto res = GetLastError();
+
+	return hr;
+}
+
+HRESULT App::InitGUI()
 {	
 	/*
 	 Fixed size 1280x700 used to get rid od of scaling issues further on
 	*/
-	m_hWndMain = CreateWindowW(m_mainClassName, m_appTitle, WS_OVERLAPPEDWINDOW,
+	m_hWndMain = CreateWindowW(m_mainWindowName, m_appTitle, WS_OVERLAPPEDWINDOW,
 		0, 0, 1280, 700, nullptr, nullptr, m_hInstCurr, nullptr);
 
+	int width, height;
+	std::tie(width, height) = m_kinectAdapter->getFrameSize();
+
+	m_hWndKinect = CreateWindowW(m_kinectViewWindowName, (LPCTSTR)NULL, WS_CHILD | WS_VISIBLE,
+		0, 0, width, height, m_hWndMain, nullptr, m_hInstCurr, nullptr);
+
 	auto res = GetLastError();
-
-	m_hWndKinect = CreateWindowW(0, m_appTitle, WS_CHILD | WS_VISIBLE,
-		CW_USEDEFAULT, 0, 512, 424, m_hWndMain, nullptr, m_hInstCurr, nullptr);
-
 
 	if (!m_hWndMain)
 	{
@@ -54,22 +94,97 @@ HRESULT App::InitUI()
 	return S_OK;
 }
 
-HRESULT App::InitializeAndRegisterMainWindowClass()
+HRESULT App::InitKinectConnection()
 {
-	m_mainWindow.style = CS_HREDRAW | CS_VREDRAW;
-	m_mainWindow.lpfnWndProc = MainWindowProcessor;
-	m_mainWindow.hInstance = m_hInstCurr;
-	m_mainWindow.lpszClassName = m_mainClassName;
-	m_mainWindow.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-	return RegisterClass(&m_mainWindow) == 0 ? E_FAIL : S_OK;
+	HRESULT hr;
+	if (m_kinectAdapter != nullptr) {
+		delete m_kinectAdapter;
+		m_kinectAdapter = nullptr;
+	}
+	m_kinectAdapter = ModeFactory::getMode(KinectMode::DEPTH);
+	hr = m_kinectAdapter != nullptr ? S_OK : E_POINTER;
+	if (SUCCEEDED(hr))
+		hr = m_kinectAdapter->initiateKinectConnection();
+	return hr;
+}
+
+HRESULT App::InitKinectRenderer()
+{
+	// Init Direct2D
+	ID2D1Factory* m_pD2DFactory;
+	D2D1CreateFactory(D2D1_FACTORY_TYPE_SINGLE_THREADED, &m_pD2DFactory);
+
+	int width, height;
+	std::tie(width, height) = m_kinectAdapter->getFrameSize();
+	m_pDepthRGBX = new RGBQUAD[width * height];
+
+	HRESULT hr = m_imageRenderer.Initialize(m_hWndKinect, m_pD2DFactory, width, height, width * sizeof(RGBQUAD));
+	
+	return hr;
 }
 
 HRESULT App::InitApp()
 {
 	HRESULT hr;
-	hr = InitializeAndRegisterMainWindowClass();
+	hr = InitAndRegisterWindowClasses();
 	if (SUCCEEDED(hr))
-		hr = InitUI();
+		hr = InitKinectConnection();
+	if (SUCCEEDED(hr))
+		hr = InitGUI();
+	if (SUCCEEDED(hr))
+		hr = InitKinectRenderer();
 
+	return hr;
+}
+
+HRESULT App::UpdateKinectImage()
+{
+	/*
+		TODO add handling of empty kinect view instance
+	*/ 
+	HRESULT hr;
+	IKinectData* res = new IKinectData();
+	hr = m_kinectAdapter->getCurrentFrame(res);
+	if (SUCCEEDED(hr)) {
+
+		// Make sure we've received valid data
+		if (m_pDepthRGBX && res->pBuffer && res->validFrame)
+		{
+			RGBQUAD* pRGBX = m_pDepthRGBX;
+
+			// end pixel is start + width*height - 1
+			const UINT16* pBufferEnd = res->pBuffer + (res->nWidth * res->nHeight);
+			auto pBuffer = res->pBuffer;
+
+			while (pBuffer < pBufferEnd)
+			{
+				USHORT depth = *pBuffer;
+
+				// To convert to a byte, we're discarding the most-significant
+				// rather than least-significant bits.
+				// We're preserving detail, although the intensity will "wrap."
+				// Values outside the reliable depth range are mapped to 0 (black).
+
+				// Note: Using conditionals in this loop could degrade performance.
+				// Consider using a lookup table instead when writing production code.
+
+				//TODO properly handle death values
+				auto depth_max = 250;
+
+				BYTE intensity = static_cast<BYTE>((depth >= res->nDepthMinReliableDistance) && (depth <= res->nDepthMaxDistance) ? (depth % depth_max) : 0);
+
+				pRGBX->rgbRed = intensity;
+				pRGBX->rgbGreen = intensity;
+				pRGBX->rgbBlue = intensity;
+
+				++pRGBX;
+				++pBuffer;
+			}
+
+			// Draw the data with Direct2D
+			m_imageRenderer.Draw(reinterpret_cast<BYTE*>(m_pDepthRGBX), res->nWidth * res->nHeight * sizeof(RGBQUAD));
+		}
+	}
+	m_kinectAdapter->releaseSpecificResources();
 	return hr;
 }
--- MovementTracker_GUI/App.h
+++ MovementTracker_GUI/App.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "framework.h"
+#include "KinectAdapter.h"
+#include "ImageRenderer.h"
+#include <d2d1.h>
 
 class App
 {
@@ -11,16 +14,30 @@ public:
 	HRESULT run();
 	~App();
 private:
+	//Initializers
+	HRESULT InitAndRegisterWindowClasses();
+	HRESULT InitGUI();
+	HRESULT InitKinectConnection();
+	HRESULT InitKinectRenderer();
 	HRESULT InitApp();
-	HRESULT InitUI();
-	HRESULT InitializeAndRegisterMainWindowClass();
+
+	//Cyclic app functions
+	HRESULT UpdateKinectImage();
 
 	HWND m_hWndMain;									// main window
 	HWND m_hWndKinect;									// window for kinect rendering
 	HINSTANCE m_hInstCurr;								// current instance
 	int m_nCmdShow;										// main window show mode
 	const WCHAR m_appTitle[18] = L"Movement recorder";
-	const WCHAR m_mainClassName[5] = L"main";
+	IKinectMode* m_kinectAdapter;
+	ImageRenderer m_imageRenderer;
+	RGBQUAD* m_pDepthRGBX;
+
+	//App window classes
+	const WCHAR m_mainWindowName[10] = L"mainClass";
 	WNDCLASS m_mainWindow;
+
+	const WCHAR m_kinectViewWindowName[11] = L"kinectView";
+	WNDCLASS m_kinectViewWindow;
 };
 
